e81e1aa941e1ea48f583e27b65c0a30b92413b59
JK3WN <yimjay1@gmail.com>
2024-07-16 20:11:34 +0900

    Changed Level Select Screen

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    public GameObject TitleSection, CreditsSection, LevelSection;
+    public GameObject TitleSection, ControlsSection, LevelSection;
+    public UnityEngine.UI.Button[] buttons;
+    public TextAsset[] MapFiles;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < buttons.Length && i < MapFiles.Length; i++)
+        {
+            int val = i;
+            buttons[i].onClick.AddListener(() => OnButtonClick(val));
+            buttons[i].interactable = true;
+        }
     }
 
     // Update is called once per frame
@@ -24,10 +32,10 @@ public class MenuManager : MonoBehaviour
         LevelSection.gameObject.SetActive(true);
     }
 
-    public void CreditsPressed()
+    public void ControlsPressed()
     {
         TitleSection.gameObject.SetActive(false);
-        CreditsSection.gameObject.SetActive(true);
+        ControlsSection.gameObject.SetActive(true);
     }
 
     public void ExitPressed()
@@ -39,7 +47,13 @@ public class MenuManager : MonoBehaviour
     public void BackPressed()
     {
         LevelSection.gameObject.SetActive(false);
-        CreditsSection.gameObject.SetActive(false);
+        ControlsSection.gameObject.SetActive(false);
         TitleSection.gameObject.SetActive(true);
+    }
+
+    public void OnButtonClick(int value)
+    {
+        Debug.Log("Button value " + value);
+        SceneManager.LoadScene("Scenes/SHScene");
     }
 }
