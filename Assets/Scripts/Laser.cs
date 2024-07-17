937dfecfe2199078696116ac27a405e004806ad2
JK3WN <yimjay1@gmail.com>
2024-07-18 01:25:21 +0900

    qnpf

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -2,7 +2,6 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UIElements;
 
